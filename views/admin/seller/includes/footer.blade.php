9dce5093d05642e9b1b6f54ce8e6b8e8e886ace8
heba201 <74704287+heba201@users.noreply.github.com>
2025-05-30 09:49:21 -0700

    Delete views/admin/seller directory

--- views/admin/seller/includes/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer">
-          <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">{!! get_setting('frontend_copyright_text',null,App::getLocale()) !!}</span>
-          </div>
-        </footer>
